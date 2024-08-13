0c77492e8a74d2295d81dcf0f30ee8a08d5b237e
AminovJamshid <aminovjamshid777@gmail.com>
2024-08-13 14:17:45 +0500

    MINOR: added web features

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+//require_once __DIR__ . '/src/DB.php';
+//
+//require_once __DIR__ . '/src/Router.php';
+//
+//require_once __DIR__ . '/src/Bot.php';
+//require_once __DIR__ . '/src/Send_massage.php';
